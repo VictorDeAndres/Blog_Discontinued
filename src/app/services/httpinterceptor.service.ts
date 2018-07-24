aef2d415900b094ed72054bc19a58a737163d684
Victor <victor@iMac-de-Capitan.local>
2018-07-24 09:52:21 +0200

    Add ga to blog

--- src/app/services/httpinterceptor.service.ts
+++ src/app/services/httpinterceptor.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+
+import ua from 'universal-analytics';
+
 @Injectable()
 export class HttpinterceptorService {
 
-  constructor() { }
+  private visitor;
+
+  constructor() {
+    this.visitor = ua('UA-111721516-1');
+  }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-   return next.handle(request);
+    this.visitor.pageview(request.url).send();
+    return next.handle(request);
   }
 }
